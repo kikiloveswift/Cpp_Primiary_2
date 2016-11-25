8e4ab5a8f973d7384e39c5bf67630535effd25ff
konglee <konglee873@gmail.com>
2016-11-25 09:24:28 +0800

    Initial Commit

---
+++ Cpp_Primiary_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp_Primiary_2
+//
+//  Created by kong on 16/11/25.
+//  Copyright © 2016年 konglee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
